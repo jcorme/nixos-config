ae4a662fd7bbb42d28bf1f5e675690d746dd9b37
Jason Chen <jason@jcndrop.com>
2021-11-15 21:50:05 -0800

    Add x86_64 binfmt_misc registration to build x86_64 containers

--- hosts/vm-aarch64.nix
+++ hosts/vm-aarch64.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ../modules/binfmt.nix
     ../modules/vmware-guest.nix
     ./vm-shared.nix
   ];
@@ -9,6 +10,14 @@
   disabledModules = [
     # Default vmware-guest module does not support aarch64.
     "virtualisation/vmware-guest.nix"
+
+    # Sets interpreter to /run/binfmt/${name}, which causes docker buildx to
+    # not see x86_64 as a supported platform for some reason.
+    "system/boot/binfmt.nix"
+  ];
+
+  nixpkgs.overlays = [
+    (import ../overlays)
   ];
 
   environment.systemPackages = [
@@ -25,6 +34,18 @@
       '';
     }
   ];
+
+  # Register qemu-x86_64 binfmt so we can build x86 container images
+  boot.binfmt.registrations = {
+    qemu-x86_64 = {
+      magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+      mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+      interpreter = "/run/current-system/sw/bin/qemu-x86_64-static";
+      preserveInterpreter = true;
+      matchCredentials = true;
+      fixBinary = true;
+    };
+  };
 
   # ens160 is the default interface with VMWare Fusion.
   networking.interfaces.ens160.useDHCP = true;
